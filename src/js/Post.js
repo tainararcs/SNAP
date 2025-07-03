9472919659cd29b0ba6cc876b111709290a925a1
Tainara <tainararobertacarvalho@gmail.com>
2025-07-03 16:40:19 -0300

    Criação das classes Interest, User e Post

--- src/js/Post.js
+++ src/js/Post.js
@@ -1,1 +1,29 @@
 // Classe de estrutura de uma postagem. 
+
+class Post {
+    userName;
+    content;
+
+    constructor(userName, content) {
+        this.userName = userName;
+        this.content = content;
+    }
+
+    // Getters.
+    getUserName() {
+        return this.userName;
+    }
+
+    getContent() {
+        return this.content;
+    }
+
+    // Setters.
+    setUserName(newUserName) {
+        this.userName = newUserName;
+    }
+
+    setContent(newContent) {
+        this.content = newContent;
+    }
+}
--- src/js/interests.js
+++ src/js/interests.js
@@ -1,1 +1,46 @@
+//
 
+class Interest {
+
+    interests = [];
+    
+    constructor(initialInterests = []) {
+        this.interests = [...initialInterests];
+    }
+
+    // Retorna um interesse com base no índice fornecido.
+    getInterest(index) {
+        if (index >= 0 && index < this.interests.length) {
+            return this.interests[index];
+        }
+        return null;        
+    }
+     
+    // Retorna a lista completa de interesses.
+    getInterests() {
+        return this.interests;
+    }
+
+    // Adiciona um interesse.
+    addInterest(interest) {
+        this.interests.push(interest);
+    }
+    
+    // Adiciona uma lista de interesses.
+    addInterestList(interestList) {
+        this.interests.push(...interestList);
+    }
+
+    // Pesquisa um interesse na lista.
+    searchInterest(interest) {
+    for (let i = 0; i < this.interests.length; i++) {
+        if (this.interests[i] === interest) {
+            return i;
+        }
+        return -1;
+    }
+    
+}
+
+
+}   
