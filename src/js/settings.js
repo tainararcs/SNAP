d3d724b1a1cb37ebc4eb455616fb5ca1a76c3816
Cássia Rodrigues <cassiaclrif@gmail.com>
2025-07-17 08:44:02 -0300

    Implementação da página de configurações.

--- src/js/settings.js
+++ src/js/settings.js
@@ -10,3 +10,51 @@ configLink.addEventListener("click", (e) => {
     showPage("page-configs");
     setActiveLink("link-configs");
 });
+
+
+document.addEventListener("click", (e) => {
+    // Voltar para a página inicial (feed)
+    if(e.target.id === 'backToFeed') {
+        e.preventDefault();
+        window.location.href = "feed.html";
+    }
+    
+    // Mostrar tema
+    if(e.target.id === 'changeTheme') {
+        document.querySelector("#settingsDesktop").style.display = "none";
+        document.querySelector("#theme-div").style.display = "block";
+    }
+    // Voltar do tema para configurações
+    if(e.target.id === 'TbackToSettings') {
+        document.querySelector("#settingsDesktop").style.display = "block";
+        document.querySelector("#theme-div").style.display = "none";
+    }
+    
+    // Mostrar ajuda
+    if(e.target.id === 'help') {
+        document.querySelector("#settingsDesktop").style.display = "none";
+        document.querySelector("#help-div").style.display = "block";
+    }
+    // Voltar da ajuda para configurações
+    if(e.target.id === 'HbackToSettings') {
+        document.querySelector("#settingsDesktop").style.display = "block";
+        document.querySelector("#help-div").style.display = "none";
+    }
+    
+    // Mostrar idioma
+    if(e.target.id === 'language') {
+        document.querySelector("#settingsDesktop").style.display = "none";
+        document.querySelector("#language-div").style.display = "block";
+    }
+    // Voltar do idioma para configurações
+    if(e.target.id === 'LbackToSettings') {
+        document.querySelector("#settingsDesktop").style.display = "block";
+        document.querySelector("#language-div").style.display = "none";
+    }
+
+    // Logout
+    if(e.target.id === 'logoutBtn') {
+        e.preventDefault();
+        window.location.href = "index.html";
+    }
+});
